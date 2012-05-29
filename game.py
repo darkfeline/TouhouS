b1334261d7e20cc18f6f4b9d8219af2ddc55f242
Allen Li <darkfeline@abagofapples.com>
2012-05-29 00:33:15 -0700

    Move handlers/update to Player class
    Move event handlers and update() to Player class for flexibility

--- game.py
+++ game.py
@@ -5,6 +5,9 @@ from __future__ import division
 import math
 
 import pyglet
+from pyglet.window import key
+
+import resources
 
 class Sprite(pyglet.sprite.Sprite):
     pass
@@ -85,6 +88,9 @@ class Player(Sprite):
     shot_state
     Current shot state (FPS and stuff)
 
+    shots
+    BulletGroup containing player shots
+
     """
 
     def __init__(self, *args, **kwargs):
@@ -95,6 +101,7 @@ class Player(Sprite):
         self.shooting = 0
         self.shot_rate = 30
         self.shot_state = 0
+        self.shots = BulletGroup()
 
     def speed(self):
         if self.focus:
@@ -102,6 +109,54 @@ class Player(Sprite):
         else:
             return self.speed_multiplier
 
+    def on_key_press(self, symbol, modifiers):
+        if symbol == key.LSHIFT:
+            self.focus = 1
+        elif symbol == key.Z:
+            self.shooting = 1
+
+    def on_key_release(self, symbol, modifiers):
+        if symbol == key.LSHIFT:
+            self.focus = 0
+        elif symbol == key.Z:
+            self.shooting = 0
+
+    def on_draw(self):
+        self.shots.draw()
+        self.draw()
+
+    def update(self, dt, keys, width, height):
+        # movement
+        x = 0
+        if keys[key.LEFT]:
+            x = -1
+        if keys[key.RIGHT]:
+            x += 1
+        y = 0
+        if keys[key.DOWN]:
+            y = -1
+        if keys[key.UP]:
+            y += 1
+        if not x == y == 0:
+            v = Vector(x, y).get_unit_vector()
+            self.x += self.speed() * v.x
+            self.y += self.speed() * v.y
+        # bullet generation
+        if self.shooting:
+            self.shot_state += dt
+            period = 1 / self.shot_rate  # period of shot
+            i = 0
+            while self.shot_state > period:
+                shot = Bullet(img=resources.shot_image, x=self.x, y=self.y)
+                v = shot.direction * shot.speed
+                v = v * i
+                shot.x += v.x
+                shot.y += v.y
+                self.shots.add(shot)
+                self.shot_state -= period
+                i += period
+        # bullet movement
+        self.shots.update(dt, width, height)
 
 class Bullet(Sprite):
 
--- touhouS.py
+++ touhouS.py
@@ -27,89 +27,18 @@ gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
 # Log events
 #window.push_handlers(pyglet.window.event.WindowEventLogger())
 
-# Batches/Groups
-shots = []
-shots_batch = pyglet.graphics.Batch()
-
 fps_display = pyglet.clock.ClockDisplay()
 player = game.Player(img=resources.player_image, x=50, y=50)
 
 player_lives = 3
 
-window.push_handlers()
+window.push_handlers(player)
+pyglet.clock.schedule_interval(player.update, FPS, keys, WIDTH, HEIGHT)
 
-@window.event
-def on_key_press(symbol, modifiers):
-    if symbol == key.LSHIFT:
-        player.focus = 1
-    elif symbol == key.Z:
-        player.shooting = 1
-
-@window.event
-def on_key_release(symbol, modifiers):
-    if symbol == key.LSHIFT:
-        player.focus = 0
-    elif symbol == key.Z:
-        player.shooting = 0
-
-@window.event
 def on_draw():
     window.clear()
     fps_display.draw()
-    shots_batch.draw()
-    player.draw()
-
-def update(dt):
-    # player movement
-    global player
-    global keys
-    x = 0
-    if keys[key.LEFT]:
-        x = -1
-    if keys[key.RIGHT]:
-        x += 1
-    y = 0
-    if keys[key.DOWN]:
-        y = -1
-    if keys[key.UP]:
-        y += 1
-    if not x == y == 0:
-        v = game.Vector(x, y).get_unit_vector()
-        player.x += player.speed() * v.x
-        player.y += player.speed() * v.y
-
-    # bullet movement
-    global shots
-    global shots_batch
-    temp = []
-    for shot in shots:
-        v = shot.direction * shot.speed
-        shot.x += v.x
-        shot.y += v.y
-        # bullet deletion
-        if shot.x < 0 or shot.x > WIDTH or shot.y < 0 or shot.y > HEIGHT:
-            shot.delete()
-        else:
-            temp.append(shot)
-    shots = temp
-
-    # bullet generation
-    if player.shooting:
-        player.shot_state += dt
-        period = 1 / player.shot_rate  # period of shot
-        i = 0
-        while player.shot_state > period:
-            shot = game.Bullet(img=resources.shot_image, x=player.x,
-                    y=player.y, batch=shots_batch)
-            v = shot.direction * shot.speed
-            v = v * i
-            shot.x += v.x
-            shot.y += v.y
-            shots.append(shot)
-            player.shot_state -= period
-            i += period
-
-pyglet.clock.schedule_interval(update, FPS)
+window.push_handlers(on_draw)
 
 if __name__ == "__main__":
     pyglet.app.run()

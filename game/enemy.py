2a14921c828d82fad4b08ee46091fa3d88dae4e5
Allen Li <darkfeline@abagofapples.com>
2012-05-30 01:29:27 -0700

    Make/complete Enemy generic class

--- game/enemy.py
+++ game/enemy.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+import pyglet
+
 from game.sprite import Sprite
-from game import resources
+from game.vector import Vector
 from game.bullet import BulletGroup, Bullet
 
 class EnemyGroup:
@@ -25,7 +27,54 @@ class EnemyGroup:
             enemy.update(dt)
         self.bullets.update(dt)
 
+
 class Enemy(Sprite):
 
-    def __init__(self, x, y):
-        super().__init__(img=resources.enemy_image, x=x, y=y)
+    def __init__(self, x, y, bullet_group, img=None, *args, **kwargs):
+        super().__init__(*args, x=x, y=y, img=img, **kwargs)
+        self.dest = Vector(x, y)
+        self.speed = 0
+        self.max_speed = 250
+        self.accel = 500
+        self.bullets = bullet_group
+
+    @property
+    def accel_dist(self):
+        return self.max_speed - self.speed
+
+    @property
+    def decel_dist(self):
+        return self.speed
+
+    @property
+    def dest(self):
+        return self._dest
+
+    @dest.setter
+    def dest(self, value):
+        self._dest = value
+        self._vector = value - Vector(self.x, self.y)
+        self._vector = self._vector.get_unit_vector()
+
+    @property
+    def vector(self):
+        return self._vector
+
+    def fire_at(self, dest):
+        dest = Vector(dest[0], dest[1])
+        v = dest - Vector(self.x, self.y)
+        b = Bullet(self.x, self.y, vector=v)
+        self.bullets.add(b)
+
+    def update(self, dt):
+        # movement
+        diff = self.dest - Vector(self.x, self.y)
+        if diff != Vector(0, 0):
+            # acceleration/deceleration
+            if diff.length <= self.decel_dist:
+                self.speed -= self.accel * dt
+            if self.speed < self.max_speed:
+                self.speed += self.accel * dt
+            # movement
+            self.x += self.speed * self.vector.x * dt
+            self.y += self.speed * self.vector.y * dt

ef739cea8d6f10456bd52dc93ffd737934a7c033
Allen Li <darkfeline@abagofapples.com>
2012-05-30 01:29:00 -0700

    Make EnemyGroup class

--- game/enemy.py
+++ game/enemy.py
@@ -2,6 +2,28 @@
 
 from game.sprite import Sprite
 from game import resources
+from game.bullet import BulletGroup, Bullet
+
+class EnemyGroup:
+
+    def __init__(self):
+        self.enemies = []
+        self.batch = pyglet.graphics.Batch()
+        self.bullets = BulletGroup()
+
+    def add(self, enemy):
+        self.enemies.append(enemy)
+        enemy.batch = self.batch
+        enemy.bullets = self.bullets
+
+    def draw(self):
+        self.batch.draw()
+        self.bullets.draw()
+
+    def update(self, dt):
+        for enemy in self.enemies:
+            enemy.update(dt)
+        self.bullets.update(dt)
 
 class Enemy(Sprite):
 

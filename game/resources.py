99e5408f6c0432a2ceb1d27a5d24631e62863eef
Allen Li <darkfeline@abagofapples.com>
2012-06-01 17:39:48 -0700

    Optimize resources.py centered image loading

--- game/resources.py
+++ game/resources.py
@@ -2,6 +2,11 @@
 
 from pyglet import resource
 
+def load_image(image):
+    image = resource.image(image)
+    center_image(image)
+    return image
+
 def center_image(image):
     """Sets an image's anchor point to center"""
     image.anchor_x = image.width // 2
@@ -13,13 +18,9 @@ resource.reindex()
 icon1 = resource.image('icon16.png')
 icon2 = resource.image('icon32.png')
 
-player_image = resource.image("player.png")
-center_image(player_image)
-shot_image = resource.image('shot.png')
-center_image(shot_image)
-enemy_image = resource.image('enemy.png')
-center_image(enemy_image)
-bullet_image = resource.image('bullet.png')
-center_image(bullet_image)
-hitbox_image = resource.image('hitbox.png')
-center_image(hitbox_image)
+
+player_image = load_image("player.png")
+shot_image = load_image('shot.png')
+enemy_image = load_image('enemy.png')
+bullet_image = load_image('bullet.png')
+hitbox_image = load_image('hitbox.png')

fa957fed2a90464756292af811af7254ccb61d43
Allen Li <darkfeline@abagofapples.com>
2012-08-19 02:54:42 -0700

    Add profiler script

---
+++ bin/profile.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import pstats, cProfile
+
+import touhouS
+
+cProfile.runctx("touhouS.main()", globals(), locals(), "Profile.prof")
+
+s = pstats.Stats("Profile.prof")
+s.strip_dirs().sort_stats("time").print_stats()

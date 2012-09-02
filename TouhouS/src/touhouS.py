0470c68384be6b8e286cef30c52ccd98d9925424
Allen Li <darkfeline@abagofapples.com>
2012-09-02 14:01:01 -0400

    New main script

--- TouhouS/src/touhouS.py
+++ TouhouS/src/touhouS.py
@@ -3,15 +3,19 @@
 import pyglet
 from pyglet.window import key
 from pyglet import gl
-
-from game import Game
-import resources
 from gensokyo.constants import WIDTH, HEIGHT, FPS
+from gensokyo import constants
+from gensokyo.view import View
+
+from model import Model
+import resources
 
 def main():
     window = pyglet.window.Window(WIDTH, HEIGHT)
+    constants.WINDOW = window
     window.set_caption('TouhouS')
     window.set_icon(resources.icon16, resources.icon32)
+    window.clear()
 
     # Transparency
     gl.glEnable(gl.GL_BLEND)
@@ -24,18 +28,17 @@ def main():
     # keys
     keys = key.KeyStateHandler()
     window.push_handlers(keys)
-    # game
-    game = Game(keys)
-    to_update.append(game)
-    window.push_handlers(game)
-    # clear screen
-    def on_draw():
-        window.clear()
-    window.push_handlers(on_draw)
+    constants.KEYS = keys
+    # view
+    view = View()
+    window.push_handlers(view)
+    constants.VIEW = view
+    # model
+    model = Model()
+    window.push_handlers(model)
 
     def update(dt):
-        for x in to_update:
-            x.update(dt)
+        model.update(dt)
     pyglet.clock.schedule_interval(update, 1./FPS)
     pyglet.clock.set_fps_limit(FPS)
 

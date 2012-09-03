855fa935eaba4877c6dd4e551544a77138c0429e
Allen Li <darkfeline@abagofapples.com>
2012-09-03 17:08:47 -0400

    Attempt #2, mostly cleanup

--- gensokyo/gensokyo/view.py
+++ gensokyo/gensokyo/view.py
@@ -24,17 +24,26 @@ class View:
 
     def on_add_sprite(self, sprite, group):
         if isinstance(sprite, Label):
-            self.set_label_group(sprite, self.groups[group])
+            self.add_label(sprite, group)
         else:
+            self.add_sprite(sprite, group)
+
+    def add_label(self, label, group):
+        set_label_group(label, self.groups[group])
+        label.batch = self.batch
+        label._own_batch = False
+
+    def add_sprite(self, sprite, group):
+        try:
             sprite.group = self.groups[group]
+        except AttributeError:  # sprite already deleted
+            return
         sprite.batch = self.batch
-        if isinstance(sprite, Label):
-            sprite._own_batch = False
 
 
-    def set_label_group(self, label, group):
-        label.top_group = TextLayoutGroup(group)
-        label.background_group = OrderedGroup(0, label.top_group)
-        label.foreground_group = TextLayoutForegroundGroup(1, label.top_group)
-        label.foreground_decoration_group = \
-                TextLayoutForegroundDecorationGroup(2, label.top_group)
+def set_label_group(label, group):
+    label.top_group = TextLayoutGroup(group)
+    label.background_group = OrderedGroup(0, label.top_group)
+    label.foreground_group = TextLayoutForegroundGroup(1, label.top_group)
+    label.foreground_decoration_group = \
+            TextLayoutForegroundDecorationGroup(2, label.top_group)

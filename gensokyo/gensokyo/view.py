11480dcc8d9817c02fb2eddac42472d20af060f2
Allen Li <darkfeline@abagofapples.com>
2012-09-03 16:35:14 -0400

    Attempt to fix Label drawing #1
    Using only one batch now, trying to properly set Label batch and group

--- gensokyo/gensokyo/view.py
+++ gensokyo/gensokyo/view.py
@@ -3,6 +3,8 @@
 import pyglet
 from pyglet.graphics import OrderedGroup
 from pyglet.text import Label
+from pyglet.text.layout import TextLayoutGroup, TextLayoutForegroundGroup
+from pyglet.text.layout import TextLayoutForegroundDecorationGroup
 
 from gensokyo import globals
 
@@ -15,16 +17,24 @@ class View:
         self.batch = pyglet.graphics.Batch()
         self.groups = dict(zip(self.__class__._map,
             [OrderedGroup(i) for i in range(len(self.__class__._map))]))
-        self.labels = pyglet.graphics.Batch()
 
     def on_draw(self):
         globals.WINDOW.clear()
         self.batch.draw()
-        self.labels.draw()
 
     def on_add_sprite(self, sprite, group):
         if isinstance(sprite, Label):
-            sprite.batch = self.labels
-            return
-        sprite.group = self.groups[group]
+            self.set_label_group(sprite, self.groups[group])
+        else:
+            sprite.group = self.groups[group]
         sprite.batch = self.batch
+        if isinstance(sprite, Label):
+            sprite._own_batch = False
+
+
+    def set_label_group(self, label, group):
+        label.top_group = TextLayoutGroup(group)
+        label.background_group = OrderedGroup(0, label.top_group)
+        label.foreground_group = TextLayoutForegroundGroup(1, label.top_group)
+        label.foreground_decoration_group = \
+                TextLayoutForegroundDecorationGroup(2, label.top_group)

f4e1e979e3b570dd02689199b0369e560a2a9b63
Allen Li <darkfeline@abagofapples.com>
2012-09-03 21:00:14 -0400

    Revamp Scenes
    New way is a SceneStack as EventDispatcher.  It will dispatch events to
    the MVCs.  Each scene contains an MVC.  Events will be dispatched down
    stack until one returns EVENT_HANDLED.  each MVC has a reference to the
    stack ("master") so they can dispatch events.
    
    The stack itself has the main window, and registers pushed MVCs that
    need it to the window.

--- gensokyo/gensokyo/scene.py
+++ gensokyo/gensokyo/scene.py
@@ -1,32 +1,67 @@
 #!/usr/bin/env python3
 
-from gensokyo import globals
+from pyglet.event import EventDispatcher
 
-class SceneStack:
+class SceneStack(EventDispatcher):
 
-    def __init__(self):
+    def __init__(self, window):
+        self.window = window
         self.stack = []
 
+    @property
+    def model(self):
+        return self.stack[-1].model
+
+    @property
+    def view(self):
+        return self.stack[-1].view
+
+    @property
+    def controller(self):
+        return self.stack[-1].controller
+
     def push(self, scene):
+
         self.stack.append(scene)
-        self.set()
+
+        scene.controller.master = self
+        scene.model.master = self
+        scene.view.master = self
+
+        self.push_handlers(scene.model)
+        self.push_handlers(scene.view)
+
+        self.window.push_handlers(scene.controller)
+        self.window.push_handlers(scene.view)
 
     def pop(self):
-        x = self.stack.pop()
-        self.set()
-        return x
 
-    def set(self):
-        self.stack[-1].set()
+        scene = self.stack.pop()
+
+        scene.controller.master = None
+        scene.model.master = None
+        scene.view.master = None
+
+        self.pop_handlers()
+        self.pop_handlers()
+
+        self.window.pop_handlers()
+        self.window.pop_handlers()
+
+        return scene
+
+    def update(self, dt):
+        self.dispatch_event('on_update', dt)
+
+SceneStack.register_event_type('on_update')
+SceneStack.register_event_type('on_key_press')
+SceneStack.register_event_type('on_key_release')
+SceneStack.register_event_type('on_add_sprite')
 
 
 class Scene:
 
-    def __init__(self, keys, model, view):
-        self.keys = keys
+    def __init__(self, controller, model, view):
+        self.controller = controller
         self.model = model
         self.view = view
-        model.register_view(view)
-
-    def set(self):
-        globals.KEYS = self.keys

b4c749c1c671fa9655ee672489da7c1775eddb32
Allen Li <darkfeline@abagofapples.com>
2012-09-05 16:54:16 -0400

    Remove redundant lines

--- src/view.py
+++ src/view.py
@@ -6,8 +6,4 @@ class MenuView(view.View):
 
     _map = ('bg', 'text')
 
-    def on_draw(self):
-        super().on_draw()
 
-    def on_add_sprite(self, sprite, group):
-        super().on_add_sprite(sprite, group)

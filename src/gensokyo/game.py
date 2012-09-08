4609f2c47ac588fced8cdb59fc4112a7dbecbedf
Allen Li <darkfeline@abagofapples.com>
2012-09-08 01:10:13 -0400

    Add gensokyo.game

---
+++ src/gensokyo/game.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+class Game:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, scene):
+        self.stack.append(scene)
+
+    def pop(self):
+        self.stack.pop()
+
+    def update(self, dt):
+        self.stack[-1].update(dt)
--- src/gensokyo/locator.py
+++ src/gensokyo/locator.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 from gensokyo.graphics import NullRenderingService
+from gensokyo.game import Game
 
 window = None
 rendering = NullRenderingService()
 key_state = None
+game = Game()

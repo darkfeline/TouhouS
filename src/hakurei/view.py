0cb357090642283eeb6ef90f1080a0f3df93f0c0
Allen Li <darkfeline@abagofapples.com>
2012-09-08 01:47:18 -0400

    Import fix

--- src/hakurei/view.py
+++ src/hakurei/view.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 
-from gensokyo import view
+from gensokyo.graphics import View
 
-class GameView(view.View):
+class GameView(View):
 
     _map = ('player', 'player_bullet', 'player_hb', 'enemy', 'item',
             'enemy_bullet', 'ui', 'ui_element')
 
 
-class MenuView(view.View):
+class MenuView(View):
 
     _map = ('bg', 'text')
-
-

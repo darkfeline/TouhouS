294efd4953cbbf07a75123c9338d2fe27a325652
Allen Li <darkfeline@abagofapples.com>
2012-09-08 00:28:58 -0400

    Clean up touhouS.py

--- src/touhouS.py
+++ src/touhouS.py
@@ -1,31 +1,33 @@
 #!/usr/bin/env python3
 
 import pyglet
-from pyglet.window import key
+from pyglet.window.key import KeyStateHandler
 from pyglet import gl
-from gensokyo.scene import Scene, SceneStack
-from gensokyo.controller import Controller
+from gensokyo import locator
+from gensokyo.graphics import RenderingService
 
 from hakurei.globals import WIDTH, HEIGHT, FPS
-from hakurei.model import MenuModel
-from hakurei.view import MenuView
 from hakurei import resources
 
 def main():
+    # window
     window = pyglet.window.Window(WIDTH, HEIGHT)
     window.set_caption('TouhouS')
     window.set_icon(resources.icon16, resources.icon32)
-    window.clear()
+    locator.window = window
 
     # Transparency
     gl.glEnable(gl.GL_BLEND)
     gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
 
-    # Scene Stack
-    stack = SceneStack(window)
+    # key_state
+    keys = KeyStateHandler()
+    window.push_handlers(keys)
+    locator.key_state = keys
 
-    # init stuff
-    stack.push(Scene(Controller(), MenuModel(), MenuView()))
+    # graphics
+    rendering = RenderingService()
+    locator.rendering = rendering
 
     pyglet.clock.schedule_interval(stack.update, 1./FPS)
     pyglet.clock.set_fps_limit(FPS)

963a96c26e0af6bd75b5de324c217faaaec3f2a5
Allen Li <darkfeline@abagofapples.com>
2012-09-19 00:42:32 -0400

    Bullet subclass Container, move stuff

--- src/hakurei/object/bullet.py
+++ src/hakurei/object/bullet.py
@@ -2,8 +2,8 @@
 
 import pyglet
 
-from gensokyo.object import GameObject, Group
 from gensokyo.primitives import Vector, Circle
+from gensokyo.object import AbstractContainer
 
 from hakurei.globals import GAME_AREA
 from hakurei import resources
@@ -31,23 +31,31 @@ class BulletGroup(Group):
         self.bullets = temp
 
 
-class Bullet(GameObject):
 
+class Bullet(AbstractContainer):
+
+    sprite_img = None
     sprite_group = 'enemy_bullet'
 
     def __init__(self, x, y, hb=None, speed=500, vector=Vector(0, 1)):
         super().__init__(x, y, hb)
-        self.speed = speed
-        self.vector = vector
-        self.dmg = 20
 
-    @property
-    def vector(self):
-        return self._vector
+        cls = self.__class__
+        p = PhysicsComponent()
+        c = CollisionComponent(x, y, cls.sprite_img.width,
+                cls.sprite_img.height)
+        g = GraphicsComponent(cls.sprite_group, img=cls.sprite_img)
 
-    @vector.setter
-    def vector(self, value):
-        self._vector = value.get_unit_vector()
+        p.v = vector
+        p.speed = speed
+
+        p.push_handlers(g)
+        p.push_handlers(c)
+        c.push_handlers(g)
+
+        self.add(p)
+        self.add(c)
+        self.add(g)
 
 
 class RoundBullet(Bullet):

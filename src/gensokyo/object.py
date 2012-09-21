17d2c3769f94f36f96fcade82f118528e36b4bd1
Allen Li <darkfeline@abagofapples.com>
2012-09-20 20:40:10 -0400

    Move physics to own module

--- src/gensokyo/object.py
+++ src/gensokyo/object.py
@@ -4,7 +4,7 @@ from pyglet.window import key
 from pyglet.event import EventDispatcher
 from pyglet.sprite import Sprite
 
-from gensokyo.primitives import Rect, Circle, Vector
+from gensokyo.primitives import Rect, Circle
 from gensokyo import locator
 
 class Container(EventDispatcher):
@@ -74,79 +74,6 @@ class CollisionComponent(EventDispatcher):
 
     def on_dy(self, dy):
         self.y += dy
-
-
-class PhysicsComponent(EventDispatcher):
-
-    """
-    Physics Component
-
-    .. attribute:: vel
-
-        velocity vector
-
-    .. attribute:: acc
-
-        acceleration vector
-
-    .. attribute:: max_speed
-
-        Caps speed after acceleration.  -1 for uncapped.
-
-    """
-
-    def __init__(self):
-        self.vel = Vector(0, 0)
-        self.acc = Vector(0, 0)
-        self.max_speed = -1
-
-    @property
-    def speed(self):
-        return self.vel.length
-
-    @speed.setter
-    def speed(self, value):
-        self.vel = self.vdir * value
-
-    @property
-    def accel(self):
-        return self.acc.length
-
-    @accel.setter
-    def accel(self, value):
-        self.acc = self.adir * value
-
-    @property
-    def vdir(self):
-        return self.vel.get_unit_vector()
-
-    @vdir.setter
-    def vdir(self, value):
-        """Set velocity direction as the same as vector"""
-        speed = self.speed
-        self.vel = vector
-        self.speed = speed
-
-    @property
-    def adir(self):
-        return self.acc.get_unit_vector()
-
-    @adir.setter
-    def adir(self, value):
-        """Set acceleration direction as the same as vector"""
-        speed = self.speed
-        self.acc = vector
-        self.speed = speed
-
-    def update(self, dt):
-        self.dispatch_event('on_dx', self.vel.x * dt)
-        self.dispatch_event('on_dy', self.vel.y * dt)
-        self.vel += self.acc * dt
-        if self.max_speed >= 0 and self.speed > self.max_speed:
-            self.speed = self.max_speed
-
-PhysicsComponent.register_event_type('on_dx')
-PhysicsComponent.register_event_type('on_dy')
 
 
 class SpriteComponent:

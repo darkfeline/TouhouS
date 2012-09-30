e1e10168a1c94c1975c971e12bf17227264c9d50
Allen Li <darkfeline@abagofapples.com>
2012-09-30 00:35:23 -0400

    Add System

--- src/gensokyo/system/__init__.py
+++ src/gensokyo/system/__init__.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+
+
+class System:
+
+    req_components = ()
+
+    @classmethod
+    def check(cls, entity):
+        """Check if entity has necessary components."""
+        to_check = list(cls.req_components)
+        for component in entity:
+            for type in to_check:
+                if isinstance(entity, type):
+                    to_check.remove(type)
+                    break
+        if len(to_check) > 0:
+            return False
+        else:
+            return True

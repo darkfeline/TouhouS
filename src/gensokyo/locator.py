d870415113ffbdb613238656db88aa5502df1de3
Allen Li <darkfeline@abagofapples.com>
2012-09-29 23:51:13 -0400

    Add to SL game props

--- src/gensokyo/locator.py
+++ src/gensokyo/locator.py
@@ -15,3 +15,11 @@ class ServiceLocator:
         self.key_state = None
         self.game = None
         self.collision = None
+
+    @property
+    def em(self):
+        return self.game.em
+
+    @property
+    def sm(self):
+        return self.game.sm

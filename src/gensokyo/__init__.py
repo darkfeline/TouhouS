ac05b92b2aef267874312f6892e1e3e21c905f30
Allen Li <darkfeline@abagofapples.com>
2012-09-30 00:58:34 -0400

    Update doc

--- src/gensokyo/__init__.py
+++ src/gensokyo/__init__.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python3
 
 """
-gensokyo
-
 Pyglet-based 2D game engine.
 
 gensokyo is designed for TouhouS, a Touhou-like shoot-em-up.
+
+gensokyo primarily uses MVC and a Component/System based design pattern.
+
+Game
+    Controller
+    SceneStack
+        Scene
+            Model
+                EntityManager
+                    Entity
+                        Component
+                SystemManager
+                    System
+            View
+
+Update
+    SceneStack
+        Scene
+            Model
+                SystemManager
+                    System
+            View
 
 """

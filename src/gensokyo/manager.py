351243dc68a836f3beba41a39b7a931f9a10278c
Allen Li <darkfeline@abagofapples.com>
2012-09-30 00:06:40 -0400

    Add Manager

--- src/gensokyo/manager.py
+++ src/gensokyo/manager.py
@@ -2,6 +2,16 @@
 
 from gensokyo import component
 
+
+class Manager:
+
+    def __init__(self):
+        self.items = set()
+
+    def add(self, item):
+        self.items.add(item)
+
+
 class EntityManager:
 
     def __init__(self):

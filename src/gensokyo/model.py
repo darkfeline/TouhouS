87310dd1c7e97e10b7575ba3ef1c6942d9a1c839
Allen Li <darkfeline@abagofapples.com>
2012-10-01 23:22:18 -0400

    Beef up Model

--- src/gensokyo/model.py
+++ src/gensokyo/model.py
@@ -8,6 +8,17 @@ class Model:
     def __init__(self):
         self.em = manager.EntityManager()
         self.sm = manager.SystemManager()
+        self.gm = manager.GroupManager()
+        self.tm = manager.TagManager()
+
+    def init(self, view):
+        pass
 
     def update(self, dt):
         self.sm.update(dt)
+
+    def on_key_press(self, symbol, modifiers):
+        pass
+
+    def on_key_release(self, symbol, modifiers):
+        pass

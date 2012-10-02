0a452e4ad3ce426d8778ad2402d22a9ba83927ec
Allen Li <darkfeline@abagofapples.com>
2012-10-02 01:03:58 -0400

    Add GameData

--- src/hakurei/component.py
+++ src/hakurei/component.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+
+
+class GameData:
+
+    def __init__(self, high_score=0, score=0, lives=3, bombs=3):
+        self.high_score = high_score
+        self.score = score
+        self.lives = lives
+        self.bombs = bombs

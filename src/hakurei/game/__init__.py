6a36740d3b3dc4033f6b5b7e73c23f24deb5b7c2
Allen Li <darkfeline@abagofapples.com>
2012-10-03 12:33:48 -0400

    Add GameBounds

--- src/hakurei/game/__init__.py
+++ src/hakurei/game/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 from gensokyo.entity import Entity
+from gensokyo.primitives import Rect
+from gensokyo import component
 
 
 class GameData:
@@ -16,3 +18,10 @@ class Wrapper(Entity):
 
     def __init__(self, component):
         self.add(component)
+
+
+class GameBounds(Entity):
+
+    def __init__(self, x, y, width, height):
+        hb = component.Hitbox(Rect(x, y, width, height))
+        self.add(hb)

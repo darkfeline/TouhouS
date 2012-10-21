3ff533c0e33da0cbd24530011fb5d8367d65ca97
Allen Li <darkfeline@abagofapples.com>
2012-10-21 15:54:21 -0400

    Migrate ai

--- src/hakurei/component.py
+++ src/hakurei/component.py
@@ -3,14 +3,6 @@
 from gensokyo import ces
 
 
-class EnemyAI(ces.Component):
-
-    def __init__(self, script):
-        self.script = script
-        self.step = 0
-        self.sleep = 0
-
-
 class Life(ces.Component):
 
     def __init__(self, life):

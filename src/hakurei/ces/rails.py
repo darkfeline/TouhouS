18e0fbdc6464b56422e79a5fab827c0a5c06e2b2
Allen Li <darkfeline@abagofapples.com>
2012-10-22 23:46:18 -0400

    Merge ai, rails -> rails, track

--- src/hakurei/ces/rails.py
+++ src/hakurei/ces/rails.py
@@ -12,25 +12,89 @@ from hakurei.ces import Position
 
 class Rails(ces.Component):
 
-    def __init__(self, rails):
-        """
-        Format for rails::
-
-            [(action, *args, until), ...]
-
-        """
-        self.rails = rails
-        self.time = 0
-
-    @property
-    def current(self):
-        cur = 0
-        while self.rails[cur][-1] < self.time:
-            cur += 1
-        return self.rails[cur][:-1]
+    def __init__(self, script):
+        self.script = script
+        self.step = 0
+        self.sleep = 0
+        self.track = None
 
 
 class RailSystem(ces.System):
+
+    req_components = (Rails,)
+    callable_methods = set()
+
+    def track(self, entity, rails, *args):
+        entity.track = args
+
+    def goto(self, entity, rails, step=0):
+
+        """
+        :param entity: entity passed to call
+        :type entity: Entity
+        :param rails: Rails component passed to call
+        :type rails: Rails
+        :param step: index in script to jump to
+        :type step: int
+
+        """
+
+        rails.step = step - 1
+    callable_methods.add(goto)
+
+    def sleep(self, entity, rails, time):
+
+        """
+        :param entity: entity passed to call
+        :type entity: Entity
+        :param rails: Rails component passed to call
+        :type rails: Rails
+        :param time: time to sleep in seconds
+        :type time: int
+
+        """
+
+        rails.sleep = time
+    callable_methods.add(sleep)
+
+    def call(self, entity, rails, method_name, *args, **kwargs):
+
+        """
+        Call method with given name and pass it the given entity
+
+        Valid methods for such calling have the type signature::
+
+            method_name(self, entity, rails, *args, **kwargs)
+
+        `*args` and `**kwargs` are optional depending on method.
+
+        :param entity: entity passed to call
+        :type entity: Entity
+        :param rails: Rails component passed to call
+        :type rails: Rails
+        :param method_name: name of method
+        :type method_name: str
+
+        """
+
+        m = getattr(self, method_name)
+        if m in self.callable_methods:
+            return m(self, entity, rails, *args, **kwargs)
+        else:
+            raise TypeError(method_name + " is not a callable method")
+
+    def update(self, dt):
+        for entity in self.get_with(self.req_components):
+            for rails in entity.get(Rails):
+                if rails.sleep > 0:
+                    rails.sleep -= dt
+                else:
+                    if rails.step < len(rails.script):
+                        self.call(entity, rails, *rails.script[rails.step])
+                        rails.step += 1
+
+
+class TrackSystem(ces.System):
 
     req_components = (Rails, Position)
     callable_methods = set()
@@ -74,5 +138,4 @@ class RailSystem(ces.System):
         for entity in self.get_with(self.req_components):
             for r in entity.get(Rails):
                 for p in entity.get(Position):
-                    p.x, p.y = self.call((p.x, p.y), dt, *r.current)
-                r.time += dt
+                    p.x, p.y = self.call((p.x, p.y), dt, *r.track)

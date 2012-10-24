b9caca9a808da9643dbfdd391b97db906127cf5a
Allen Li <darkfeline@abagofapples.com>
2012-10-24 16:39:56 -0400

    Rename/restructure rails rails+track -> rails/script

--- src/hakurei/ces/rails.py
+++ src/hakurei/ces/rails.py
@@ -11,71 +11,66 @@ from gensokyo import locator
 from hakurei.ces import Position
 
 
-class Rails(ces.Component):
+class Script(ces.Component):
 
     def __init__(self, script):
         self.script = script
         self.step = 0
         self.sleep = 0
-        self.track = None
 
 
-class RailSystem(ces.System):
+class ScriptSystem(ces.System):
 
-    req_components = (Rails,)
+    req_components = (Script,)
     callable_methods = set()
 
-    def track(self, entity, rails, *args):
-        entity.track = args
-    callable_methods.add(track)
-
-    def goto(self, entity, rails, step=0):
+    def goto(self, entity, script, step=0):
 
         """
         :param entity: entity passed to call
         :type entity: Entity
-        :param rails: Rails component passed to call
-        :type rails: Rails
+        :param script: Script component passed to call
+        :type script: Script
         :param step: index in script to jump to
         :type step: int
 
         """
 
-        rails.step = step - 1
+        script.step = step - 1
     callable_methods.add(goto)
 
-    def sleep(self, entity, rails, time):
+    def sleep(self, entity, script, time):
 
         """
         :param entity: entity passed to call
         :type entity: Entity
-        :param rails: Rails component passed to call
-        :type rails: Rails
+        :param script: Script component passed to call
+        :type script: Script
         :param time: time to sleep in seconds
         :type time: int
 
         """
 
-        rails.sleep = time
+        script.sleep = time
     callable_methods.add(sleep)
 
-    def die(self, entity, rails):
+    def die(self, entity, script):
         """
         :param entity: entity passed to call
         :type entity: Entity
-        :param rails: Rails component passed to call
-        :type rails: Rails
+        :param script: Script component passed to call
+        :type script: Script
 
         """
         locator.em.delete(entity)
     callable_methods.add(die)
 
-    def fire(self, entity, rails, bullet):
+    def fire(self, entity, script, bullet):
         """
         :param entity: entity passed to call
         :type entity: Entity
-        :param rails: Rails component passed to call
-        :type rails: Rails
+        :param script: Script component passed to call
+        :type script: Script
         :param bullet: Bullet constructor
         :type bullet: callable returning Bullet
 
@@ -84,21 +79,21 @@ class RailSystem(ces.System):
         locator.gm.add_to(b)
     callable_methods.add(fire)
 
-    def call(self, entity, rails, method_name, *args, **kwargs):
+    def call(self, entity, script, method_name, *args, **kwargs):
 
         """
         Call method with given name and pass it the given entity
 
         Valid methods for such calling have the type signature::
 
-            method_name(self, entity, rails, *args, **kwargs)
+            method_name(self, entity, script, *args, **kwargs)
 
         `*args` and `**kwargs` are optional depending on method.
 
         :param entity: entity passed to call
         :type entity: Entity
-        :param rails: Rails component passed to call
-        :type rails: Rails
+        :param script: Script component passed to call
+        :type script: Script
         :param method_name: name of method
         :type method_name: str
 
@@ -106,41 +101,48 @@ class RailSystem(ces.System):
 
         m = getattr(self, method_name)
         if m in self.callable_methods:
-            return m(self, entity, rails, *args, **kwargs)
+            return m(self, entity, script, *args, **kwargs)
         else:
             raise TypeError(method_name + " is not a callable method")
 
     def update(self, dt):
         for entity in self.get_with(self.req_components):
-            for rails in entity.get(Rails):
-                if rails.sleep > 0:
-                    rails.sleep -= dt
+            for script in entity.get(Script):
+                if script.sleep > 0:
+                    script.sleep -= dt
                 else:
-                    if rails.step < len(rails.script):
-                        self.call(entity, rails, *rails.script[rails.step])
-                        rails.step += 1
+                    if script.step < len(script.script):
+                        self.call(entity, script, *script.script[script.step])
+                        script.step += 1
 
 
-class TrackSystem(ces.System):
+class Rails(ces.Component):
+
+    def __init__(self, rails):
+        self.rails = rails
+
+
+# TODO fix this
+class RailSystem(ces.System):
 
     req_components = (Rails, Position)
     callable_methods = set()
 
-    def linear(self, pos, dt, speed, vector):
+    def straight(self, pos, dt, speed, vector):
         pos = primitives.Vector(*pos)
         move = vector.get_unit_vector()  # make a copy
         move.length = move.length * dt * speed
         pos += move
         return (pos.x, pos.y)
-    callable_methods.add(linear)
+    callable_methods.add(straight)
 
-    def circle(self, pos, dt, center, angular_vel):
+    def pivot(self, pos, dt, center, angular_vel):
         pos = primitives.Vector(*pos)
         a = (pos - center)
         a.angle += angular_vel * dt
         pos = center + a
         return (pos.x, pos.y)
-    callable_methods.add(circle)
+    callable_methods.add(pivot)
 
     def curve(self, pos, dt, speed, center):
         pass

c9ad3eb785c25e0bd98612ea6124801b17a49781
Allen Li <darkfeline@abagofapples.com>
2012-10-26 11:50:01 -0400

    Rename private funcs

--- setup.py
+++ setup.py
@@ -18,20 +18,20 @@ else:
 
 def get_resources(dir):
     l = []
-    rget_resources(dir, l)
+    _rget_resources(dir, l)
     return l
 
 
-def make_listing(dir):
+def _make_listing(dir):
     return (dir, [os.path.join(dir, x) for x in os.listdir(dir) if
             os.path.isfile(os.path.join(dir, x))])
 
 
-def rget_resources(dir, l):
-    l.append(make_listing(dir))
+def _rget_resources(dir, l):
+    l.append(_make_listing(dir))
     for d in os.listdir(dir):
         if os.path.isdir(os.path.join(dir, d)):
-            rget_resources(os.path.join(dir, d), l)
+            _rget_resources(os.path.join(dir, d), l)
 
 
 def get_modules(dir):
@@ -41,16 +41,16 @@ p_py = re.compile(r'(.+)\.py')
 
 def get_packages(dir):
     l = []
-    rget_packages(dir, '', l)
+    _rget_packages(dir, '', l)
     return l
 
 
-def rget_packages(start, dir, l):
+def _rget_packages(start, dir, l):
     ls = os.listdir(os.path.join(start, dir))
     if '__init__.py' in ls:
         l.append(dir.replace('/', '.'))
     for x in [x for x in ls if os.path.isdir(os.path.join(start, dir, x))]:
-        rget_packages(start, os.path.join(dir, x), l)
+        _rget_packages(start, os.path.join(dir, x), l)
 
 setup(
     name='TouhouS',

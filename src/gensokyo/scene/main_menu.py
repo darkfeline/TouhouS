c54f232155931f4e8b96f2b592af785184d68b9d
Allen Li <darkfeline@abagofapples.com>
2012-11-19 17:43:39 -0500

    Add logging

--- src/gensokyo/scene/main_menu.py
+++ src/gensokyo/scene/main_menu.py
@@ -1,17 +1,23 @@
+import logging
+
 from gensokyo import locator
 from gensokyo import scene
 from gensokyo.scene import game
 from gensokyo.ces import graphics
 
+logger = logging.getLogger(__name__)
+
 
 class MenuScene(scene.Scene):
 
     def init(self):
+        logger.info("Initializing MenuScene...")
         self.title = graphics.Label(
             x=20, y=globals.HEIGHT - 30, text="Welcome to TouhouS",
             color=(255, 255, 255, 255))
 
     def on_key_press(self, symbol, modifiers):
+        logger.info("Adding GameScene...")
         locator.scene_stack.push(game.GameScene())
 
 

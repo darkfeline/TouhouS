2e2300604f5d0dbbf0802713151c659605c7cb12
Allen Li <darkfeline@abagofapples.com>
2012-11-23 00:18:25 -0500

    Change script again; hopefully good this time

--- src/gensokyo/ces/script.py
+++ src/gensokyo/ces/script.py
@@ -7,28 +7,19 @@ Units
 *****
 
 The basic data structure is a *scripting unit*.  Scripting units are tuples
-containing *condition units*.  Condition units are also tuples like so::
+containing ConditionUnits.
 
-    (if, then, then, ...)
+ConditionUnits have two main properties and one method.
 
-``if`` is a function that returns Boolean.  It is followed by any number of
-functions (``then`` in the above example) which are called if the ``if`` is
-``True``.  If ``then`` returns a scripting unit, it will be added to the
-Script's list of active units.  Otherwise, ``then`` should return ``None``.  If
-it returns a list of scripting units, all of them will be added.  Newly added
-units will not be run until the next loop.
+``satisfied`` returns a Boolean.  ``run`` is a method which is called with the
+owning entity as an argument if ``satisfied`` is ``True``.  If ``then`` returns
+a scripting unit, it will be added to the Script's list of active units.
+Otherwise, ``then`` should return ``None``.  If it returns a list of scripting
+units, all of them will be added.  Newly added units will not be run until the
+next loop.
 
-If ``then`` is the constant ``EXPIRE``, the scripting unit will be expired.
-This is immediate, so make sure it is the last item in its condition unit.
-
-``if`` and ``then`` are passed their entity as their first argument.
-
-``if`` and ``then`` are example names; please do not use them (``if`` is a
-Python keyword).
-
-Scripting is functional; it evaluates only.  Thus, build the state checking
-into the functions (i.e., "currying").  If you need to store data, store it in
-the Script component.
+If ``expire`` is ``True`` (False by default), the scripting unit will be
+expired.  This is immediate; following ConditionUnits will not be processed.
 
 Scripts and ScriptSystem
 ************************
@@ -40,23 +31,33 @@ split responsibilities, possibly.
 
 ScriptSystem instances iterate over Script objects and iterate over their
 active scripting units every update loop.  Scripting units function like
-datafied if/elses::
-
-    (
-        (if, then_do_this, and_this),
-        (else_if, do_this)
-    )
+datafied if/elses.
 
 The conditions in each scripting unit are exclusive, evaluated in order.
 
-.. data:: EXPIRE
-
 """
+
+import abc
 
 from gensokyo import ces
 from gensokyo import locator
 
-EXPIRE = 'expire'
+
+class ConditionComponent:
+
+    __meta__ = abc.ABCMeta
+
+    @abc.abstractproperty
+    def satisfied(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def run(self, entity):
+        raise NotImplementedError
+
+    @property
+    def expire(self):
+        return False
 
 
 class Script(ces.Component):
@@ -79,16 +80,16 @@ class ScriptSystem(ces.System):
             for script in entity.get(self.req_components[0]):
                 for unit in list(script.units):
                     for cond in unit:
-                        test, run = cond[0], cond[1:]
-                        if test(entity):
-                            for f in run:
-                                if f == EXPIRE:
-                                    script.units.remove(unit)
-                                else:
-                                    r = f(entity)
-                                    if r:
-                                        try:
-                                            script.units.extend(r)
-                                        except TypeError:
-                                            assert isinstance(r, tuple)
-                                            script.units.append(r)
+                        if hasattr(cond, "update"):
+                            cond.update(dt)
+                        if cond.satisfied:
+                            r = cond.run(entity)
+                            if r:
+                                try:
+                                    script.units.extend(r)
+                                except TypeError:
+                                    assert isinstance(r, tuple)
+                                    script.units.append(r)
+                            if cond.expire:
+                                script.units.remove(unit)
+                                break

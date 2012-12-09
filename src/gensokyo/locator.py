351002768dca089e205a7068359ceb8932e23801
Allen Li <darkfeline@abagofapples.com>
2012-12-09 01:44:22 -0500

    Gut locator

--- src/gensokyo/locator.py
+++ src/gensokyo/locator.py
@@ -14,28 +14,8 @@ class ServiceLocator:
 
     def __init__(self):
         self.key_state = None
-        self.scene_stack = None
-        self.broadcast = None
+        self.state_tree = None
         self.window = None
-
-    @property
-    def scene(self):
-        return self.scene_stack.top
-
-    @property
-    def em(self):
-        return self.scene.em
-
-    @property
-    def tm(self):
-        return self.scene.tm
-
-    @property
-    def gm(self):
-        return self.scene.gm
-
-    @property
-    def sm(self):
-        return self.scene.sm
+        self.graphics = None
 
 sys.modules[__name__] = ServiceLocator()

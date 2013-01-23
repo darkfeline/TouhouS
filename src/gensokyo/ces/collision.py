69073e0f54c2d9d6492d224a2d94ec8b096d8bf9
Allen Li <darkfeline@abagofapples.com>
2013-01-22 19:59:15 -0500

    Fix collision

--- src/gensokyo/ces/collision.py
+++ src/gensokyo/ces/collision.py
@@ -21,6 +21,10 @@ class CollisionSystem(ces.System, metaclass=abc.ABCMeta):
 
     def on_update(self, dt):
         entities = ces.intersect(self.env, pos.Position, Hitbox)
+        p = self.env.cm[pos.Position]
+        hb = self.env.cm[Hitbox]
+        for e in entities:
+            hb[e].setpos(p[e].pos)
         self.process(entities)
 
     @abc.abstractmethod

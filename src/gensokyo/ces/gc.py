626b0bafa0b1b7b940293f84403bbbeaac15262d
Allen Li <darkfeline@abagofapples.com>
2013-01-22 21:37:53 -0500

    New gc

--- src/gensokyo/ces/gc.py
+++ src/gensokyo/ces/gc.py
@@ -4,60 +4,49 @@ Garbage collecting module
 This handles deletion of entities which are bounded by the play area, such as
 bullets.  When these entities move out of bounds, they will be deleted.
 
-Add a Presence to entities that need this feature.  A Presence has a Rect that
-will be used to determine if the entity has completely exited the boundaries.
-
 """
 
 import logging
 
 from gensokyo import ces
+from gensokyo.ces.pos import Position
 
 logger = logging.getLogger(__name__)
 
 
-class Presence(ces.Position):
+class Presence(ces.Component):
 
     """Used for garbage collecting out-of-bounds entities"""
 
     def __init__(self, rect):
         self.rect = rect
 
-    @property
-    def pos(self):
-        return self.rect.center
-
-    @pos.setter
-    def pos(self, value):
+    def setpos(self, value):
         self.rect.center = value
 
 
 class GarbageCollectSystem(ces.System):
 
-    req_components = (Presence,)
-
-    def __init__(self, env, area):
-        super().__init__(env)
-        env.clock.push_handlers(self)
+    def __init__(self, world, area):
+        super().__init__(world)
         self.area = area
 
-    def _check_bounds(self, entity):
-        """Return True if entity has a Presence component outside of bounds"""
-        c = entity.get(Presence)
-        if len(c) < 1:
-            raise NotImplementedError
-        for r in [a.hb for a in c]:
-            if (r.bottom > self.area.top or
-                    r.top < self.area.bottom or
-                    r.left > self.area.right or
-                    r.right < self.area.left):
-                return True
+    def _check_bounds(self, rect):
+        assert len(rect) == 2
+        if (rect.bottom > self.area.top or
+                rect.top < self.area.bottom or
+                rect.left > self.area.right or
+                rect.right < self.area.left):
+            return True
         return False
 
     def on_update(self, dt):
-        entities = self.env.em.get_with(self.req_components)
+        entities = ces.intersect(self.world, Position, Presence)
         logger.debug('gc found %s', entities)
+        pos = self.world.cm[Position]
+        pres = self.world.cm[Presence]
         for e in entities:
-            if self._check_bounds(e):
+            pres[e].setpos(pos[e].pos)
+            if self._check_bounds(pres[e].rect):
                 logger.debug('deleting %s', e)
-                self.env.em.delete(e)
+                self.world.remove_entity(e)

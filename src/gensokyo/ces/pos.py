4eed0f91d94bf5958829f30be0ba63a381adb02b
Allen Li <darkfeline@abagofapples.com>
2013-02-19 00:24:02 -0500

    Add SlavePosition

--- src/gensokyo/ces/pos.py
+++ src/gensokyo/ces/pos.py
@@ -1,3 +1,5 @@
+import abc
+
 from gensokyo import ces
 
 
@@ -5,3 +7,12 @@ class Position(ces.Component):
 
     def __init__(self, pos):
         self.pos = pos
+
+
+class SlavePosition(ces.Component, metaclass=abc.ABCMeta):
+
+    """Virtual class for components that need to update with Position"""
+
+    @abc.abstractmethod
+    def setpos(self, pos):
+        raise NotImplementedError

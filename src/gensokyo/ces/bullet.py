ca1a02a93bf1150b97bbaabb445864d31c77beca
Allen Li <darkfeline@abagofapples.com>
2013-02-19 18:07:38 -0500

    Refactor bullet

--- src/gensokyo/ces/bullet.py
+++ src/gensokyo/ces/bullet.py
@@ -4,7 +4,7 @@ from functools import partial
 from gensokyo import primitives
 from gensokyo.ces import collision
 from gensokyo.ces import physics
-from gensokyo.ces import graphics
+from gensokyo.ces import sprite
 from gensokyo.ces import gc
 from gensokyo import resources
 
@@ -24,8 +24,8 @@ def make_bullet(world, bullet, x, y, v):
     hb.setpos((x, y))
     world.add_component(e, hb)
 
-    sprite = graphics.Sprite(bullet.group, bullet.img, x=x, y=y)
-    world.add_component(e, sprite)
+    sprite_ = sprite.Sprite(bullet.group, bullet.img, x=x, y=y)
+    world.add_component(e, sprite_)
 
     vel = physics.Velocity(v)
     world.add_component(e, vel)

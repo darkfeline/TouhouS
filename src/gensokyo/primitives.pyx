817e4d7198cc31ef72f241b0e1b464064012df4c
Allen Li <darkfeline@abagofapples.com>
2013-03-25 18:46:53 -0400

    Change some stuff in primitives

--- src/gensokyo/primitives.pyx
+++ src/gensokyo/primitives.pyx
@@ -4,6 +4,11 @@
 
 import math
 
+try:
+    Numbers = (int, float, complex)
+except NameError:
+    Numbers = (int, float)
+
 cdef int _collide_circle_rect(Circle circle, Rect rect) except? -1:
     if rect.left < circle.x < rect.right: return True
     if rect.bottom < circle.y > rect.top: return True
@@ -30,133 +35,113 @@ cdef class Rect:
         return 'Rect({}, {}, {}, {})'.format(self.x, self.y, self.w,
                 self.h)
 
-    def __richcmp__(self, other, int i):
-        if i == 2:
-            if (self.x == other.x and self.y == other.y and 
-                    self.w == other.w and self.h == other.h):
-                return True
-            else:
-                return False
-        elif i == 3:
-            return not (self == other)
+    def __richcmp__(x, y, int op):
+        if not isinstance(x, Rect) or not isinstance(y, Rect):
+            return NotImplemented
+        if op == 2:
+            return x.x == y.x and x.y == y.y and x.w == y.w and x.h == y.h
+        elif op == 3:
+            return x.x != y.x or x.y != y.y or x.w != y.w or x.h != y.h
         else:
-            raise NotImplementedError
+            return NotImplemented
+
+    def __mul__(x, y):
+        if isinstance(x, Rect):
+            self, other = x, y
+        else:
+            self, other = y, x
+        if isinstance(other, Numbers):
+            return Rect(self.x, self.y, self.w * other, self.h * other)
+        else:
+            return NotImplemented
 
     property width:
-
         def __get__(self):
             return self.w
-
         def __set__(self, value):
             self.w = value
 
     property height:
-
         def __get__(self):
             return self.h
-
         def __set__(self, value):
             self.h = value
 
     property size:
-
         def __get__(self):
             return (self.w, self.h)
-
         def __set__(self, value):
             self.w, self.h = value
 
     property centerx:
-
         def __get__(self):
             return self.w // 2 + self.x
-
         def __set__(self, value):
             self.x += value - self.centerx
 
     property centery:
-
         def __get__(self):
             return self.h // 2 + self.y
-
         def __set__(self, value):
             self.y += value - self.centery
 
     property center:
-
         def __get__(self):
             return (self.centerx, self.centery)
-
         def __set__(self, other):
             self.centerx, self.centery = other
 
     property top:
-
         def __get__(self):
             return self.y + self.h
-
         def __set__(self, value):
             self.y += value - self.y - self.h
 
     property bottom:
-
         def __get__(self):
             return self.y
-
         def __set__(self, value):
             self.y += value - self.y
 
     property left:
-
         def __get__(self):
             return self.x
-
         def __set__(self, value):
             self.x += value - self.x
 
     property right:
-
         def __get__(self):
             return self.x + self.w
-
         def __set__(self, value):
             self.x += value - self.x - self.w
 
     property topleft:
-
         def __get__(self):
             return (self.x, self.y + self.h)
-
         def __set__(self, value):
             a = self.topleft
             self.x += value[0] - a[0]
             self.y += value[1] - a[1]
 
     property topright:
-
         def __get__(self):
             return (self.x + self.w, self.y + self.h)
-
         def __set__(self, value):
             a = self.topright
             self.x += value[0] - a[0]
             self.y += value[1] - a[1]
 
     property bottomleft:
-
         def __get__(self):
             return (self.x, self.y)
-
         def __set__(self, value):
             a = self.bottomleft
             self.x += value[0] - a[0]
             self.y += value[1] - a[1]
 
     property bottomright:
-
         def __get__(self):
             return (self.x + self.w, self.y)
-
         def __set__(self, value):
             a = self.bottomright
             self.x += value[0] - a[0]
@@ -165,7 +150,7 @@ cdef class Rect:
     def copy(self):
         return self.__class__(self.x, self.y, self.w, self.h)
 
-    cpdef int collide(self, other) except? -1:
+    def collide(self, other):
         if isinstance(other, Rect):
             if other.right < self.left: return False
             if other.left > self.right: return False
@@ -188,6 +173,33 @@ cdef class Circle:
         self.y = y
         self.r = r
 
+    def __richcmp__(self, other, int i):
+        if i == 2:
+            if isinstance(other, Circle):
+                if (self.x, self.y, self.r) == (other.x, other.y, other.r):
+                    return True
+                else:
+                    return False
+            else:
+                return NotImplemented
+        elif i == 3:
+            return not (self == other)
+        else:
+            return NotImplemented
+
+    def __mul__(x, y):
+        if isinstance(x, Circle):
+            self, other = x, y
+        else:
+            self, other = y, x
+        if isinstance(other, Numbers):
+            return Circle(self.x, self.y, self.r * other)
+        else:
+            return NotImplemented
+
+    def copy(self):
+        return self.__class__(self.x, self.y, self.r)
+
     def collide(self, other):
         if isinstance(other, Circle):
             dist = Vector(other.x - self.x, other.y - self.y)
@@ -200,29 +212,6 @@ cdef class Circle:
         else:
             raise NotImplementedError
 
-    def __richcmp__(self, other, int i):
-        if i == 2:
-            if isinstance(other, Circle):
-                if (self.x, self.y, self.r) == (other.x, other.y, other.r):
-                    return True
-                else:
-                    return False
-            else:
-                raise NotImplemented
-        elif i == 3:
-            return not (self == other)
-        else:
-            raise NotImplementedError
-
-    def __mul__(self, other):
-        if isinstance(other, int):
-            return Circle(self.x, self.y, self.r * other)
-        else:
-            raise NotImplemented
-
-    def copy(self):
-        return self.__class__(self.x, self.y, self.r)
-
 
 cdef class Vector:
 
@@ -232,43 +221,6 @@ cdef class Vector:
     def __cinit__(self, double x, double y):
         self.x = x
         self.y = y
-
-    property length:
-
-        def __get__(self):
-            return math.sqrt(self.x ** 2 + self.y ** 2)
-
-        def __set__(self, value):
-            v = self.get_unit_vector()
-            v = v * value
-            self.x, self.y = v.x, v.y
-
-
-    property angle:
-
-        def __get__(self):
-            if self.x == 0:
-                if self.y > 0:
-                    return math.pi / 2
-                elif self.y < 0:
-                    return math.pi * 3 / 2
-                else:
-                    raise NoAngleError()
-            else:
-                return math.fmod(math.atan2(self.y, self.x) + 2 * math.pi,
-                        2 * math.pi)
-
-        def __set__(self, value):
-            l = self.length
-            self.x = l * math.cos(value)
-            self.y = l * math.sin(value)
-
-    def get_unit_vector(self):
-        try:
-            t = self.angle
-        except NoAngleError:
-            return Vector(0, 0)
-        return Vector(math.cos(t), math.sin(t))
 
     def __iter__(self):
         return iter((self.x, self.y))
@@ -289,37 +241,70 @@ cdef class Vector:
         else:
             raise IndexError
 
-    def __add__(self, other):
-        if not isinstance(other, Vector):
-            raise NotImplemented
-        return Vector(self.x + other.x, self.y + other.y)
+    def __add__(x, y):
+        if not isinstance(x, Vector) or not isinstance(y, Vector):
+            return NotImplemented
+        return Vector(x.x + y.x, x.y + y.y)
 
-    def __sub__(self, other):
-        if not isinstance(other, Vector):
-            raise NotImplemented
-        return Vector(self.x - other.x, self.y - other.y)
+    def __sub__(x, y):
+        if not isinstance(x, Vector) or not isinstance(y, Vector):
+            return NotImplemented
+        return Vector(x.x - y.x, x.y - y.y)
 
-    def __richcmp__(self, other, int i):
-        if i == 2:
-            if isinstance(other, Vector):
-                if (self.x, self.y) == (other.x, other.y):
-                    return True
-                else:
-                    return False
-            else:
-                raise NotImplementedError
-        elif i == 3:
-            return not (self == other)
+    def __richcmp__(x, y, int op):
+        if not isinstance(x, Vector) or not isinstance(y, Vector):
+            return NotImplemented
+        if op == 2:
+            return x.x == y.x and x.y == y.y
+        elif op == 3:
+            return x.x != y.x or x.y != y.y
         else:
-            raise NotImplementedError
+            return NotImplemented
 
-    def __mul__(self, other):
-        if not (isinstance(other, int) or isinstance(other, float)):
-            raise NotImplemented
-        return Vector(self.x * other, self.y * other)
+    def __mul__(x, y):
+        if isinstance(x, Vector):
+            self, other = x, y
+        else:
+            self, other = y, x
+        if isinstance(other, Numbers):
+            return Vector(self.x * other, self.y * other)
+        else:
+            return NotImplemented
 
     def __str__(self):
         return "Vector({}, {})".format(self.x, self.y)
+
+    property length:
+        def __get__(self):
+            return math.sqrt(self.x ** 2 + self.y ** 2)
+        def __set__(self, value):
+            angle = self.angle
+            self.x = value * math.cos(angle)
+            self.y = value * math.sin(angle)
+
+    property angle:
+        def __get__(self):
+            if self.x == 0:
+                if self.y > 0:
+                    return math.pi / 2
+                elif self.y < 0:
+                    return math.pi * 3 / 2
+                else:
+                    raise NoAngleError
+            else:
+                return math.fmod(math.atan2(self.y, self.x) + 2 * math.pi,
+                        2 * math.pi)
+        def __set__(self, value):
+            l = self.length
+            self.x = l * math.cos(value)
+            self.y = l * math.sin(value)
+
+    def get_unit_vector(self):
+        try:
+            t = self.angle
+        except NoAngleError:
+            return Vector(0, 0)
+        return Vector(math.cos(t), math.sin(t))
 
     def copy(self):
         return self.__class__(self.x, self.y)

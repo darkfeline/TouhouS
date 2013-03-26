3619c6bd06aae12371f88215d7216178ab40565e
Allen Li <darkfeline@abagofapples.com>
2013-03-25 20:50:48 -0400

    New collision

--- src/gensokyo/ces/collision.py
+++ src/gensokyo/ces/collision.py
@@ -7,8 +7,9 @@ from gensokyo.ces import pos
 
 class Hitbox(pos.SlavePosition):
 
-    def __init__(self, hb):
+    def __init__(self, master, hb):
         self.hb = hb
+        super().__init__(master)
 
     def setpos(self, value):
         if isinstance(self.hb, primitives.Circle):
@@ -19,14 +20,6 @@ class Hitbox(pos.SlavePosition):
 
 class CollisionSystem(ces.System, metaclass=abc.ABCMeta):
 
-    def on_update(self, dt):
-        entities = ces.intersect(self.world, pos.Position, Hitbox)
-        p = self.world.cm[pos.Position]
-        hb = self.world.cm[Hitbox]
-        for e in entities:
-            hb[e].setpos(p[e].pos)
-        self.process(entities)
-
     @abc.abstractmethod
-    def process(self, entities):
+    def on_update(self, dt):
         raise NotImplementedError

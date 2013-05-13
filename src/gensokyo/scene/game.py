9320974311e30eab2206fa8b944d5042ae0b616f
Allen Li <darkfeline@abagofapples.com>
2013-05-12 23:11:45 -0400

    Finish game

--- src/gensokyo/scene/game.py
+++ src/gensokyo/scene/game.py
@@ -1,9 +1,11 @@
 import logging
+import weakref
 
 from gensokyo import state
 from gensokyo.clock import Clock
 from gensokyo import ces
 from gensokyo import sprite
+from gensokyo import stage
 from gensokyo.ces import player
 from gensokyo import ui
 from gensokyo.ces import script
@@ -21,21 +23,24 @@ logger = logging.getLogger(__name__)
 class GameScene(state.State):
 
     player_class = player.Reimu
-    stage_class = None
+    stage_class = stage.StageOne
     ui_image = resources.ui_image
 
     def __init__(self, rootenv):
 
-        self.graphics = GameDrawer()
+        super().__init__(rootenv)
+        self.drawer = GameDrawer()
         self.clock = Clock()
         self.world = ces.World()
+        self.stage = self.stage_class(rootenv, self.world)
+        self.clock.push_handlers(self.stage)
 
-        # TODO
-        # FPS
-        fps = ui.FPSDisplay(570, 2)
-        self.em.add(fps)
-        self.tm.tag('fps_display', fps)
+        #######################################################################
+        # UI
+        # UI image
+        self.ui_img = sprite.Sprite(self.drawer, 'ui', self.ui_image)
         # Counters
+        self.counters = {}
         counters = {
             'high_score': (ui.TextCounter, 430, 415, 'High score'),
             'score': (ui.TextCounter, 430, 391, 'Score'),
@@ -43,24 +48,19 @@ class GameScene(state.State):
             'bombs': (ui.IconCounter, 430, 339, 'Bombs')}
         for tag, a in counters.items():
             c, x, y, tit = a
-            counter = c(x, y, tit)
-            self.em.add(counter)
-            self.tm.tag(tag, counter)
-        # Stage
-        self.em.add(self.stage_class())
-        #######################################################################
-        # UI
-        # UI image
-        self.ui_img = sprite.Sprite(self.graphics, 'ui', self.ui_image)
+            counter = c(self.drawer, x, y, tit)
+            self.counters[tag] = counter
+        # FPS
+        self.fps = ui.FPSDisplay(self.drawer, 570, 2)
+        self.clock.push_handlers(self.fps)
 
         #######################################################################
         # Systems
-        a = player.InputMovementSystem(self.world, globals.GAME_AREA)
-        self.input = a
-        self.clock.push_handlers(a)
-        a = script.ScriptSystem(self.world)
-        self.script = a
-        self.clock.push_handlers(a)
+        self.input = player.InputMovementSystem(
+            self.world, rootenv.key_state, globals.GAME_AREA)
+        self.clock.push_handlers(self.input)
+        self.script = script.ScriptSystem(self.world, rootenv)
+        self.clock.push_handlers(self.script)
         a = GameCollisionSystem(self.world)
         self.clock.push_handlers(a)
         a = rails.RailSystem(self.world)
@@ -81,22 +81,30 @@ class GameScene(state.State):
         )
         self.world.tm['player'] = player_
 
-    def enter(self, rootenv):
+    def enter(self):
         logger.debug("Entering game")
-        rootenv.clock.push_handlers(on_update=self.clock.tick)
-        rootenv.drawers.push(self.graphics)
-        self.input.key_state = rootenv.key_state
-        self.script.rootenv = rootenv
+        self.rootenv.clock.push_handlers(on_update=self.clock.tick)
+        self.rootenv.drawers.add(self.drawer)
 
-    def exit(self, rootenv):
+    def exit(self):
         logger.debug("Exiting game")
-        rootenv.clock.remove_handlers(on_update=self.clock.tick)
-        rootenv.drawers.remove(self.graphics)
-        self.input.key_state = None
-        self.script.rootenv = None
+        self.rootenv.clock.remove_handlers(on_update=self.clock.tick)
+        self.rootenv.drawers.remove(self.drawer)
+
+    # TODO
+    def kill_player(self):
+        l = self.counters['lives']
+        if l.value > 0:
+            l.value -= 1
+        else:
+            self.rootenv.state.event('quit')
 
 
 class GameCollisionSystem(collision.CollisionSystem):
+
+    def __init__(self, world, scene):
+        super().__init__(world)
+        self.scene = weakref.ref(scene)
 
     def on_update(self, dt):
         pl = self.world.tm['player']
@@ -104,7 +112,7 @@ class GameCollisionSystem(collision.CollisionSystem):
         pl_hb = hbs[pl]
         for b in iter(self.world.gm['enemy_bullet']):
             if pl_hb.collide(hbs[b]):
-                kill_player()
+                self.scene.kill_player()
         life = self.world.cm[enemy.Life]
         for b in iter(self.world.gm['player_bullet']):
             b_hb = hbs[b]
@@ -119,12 +127,3 @@ class GameDrawer(sprite.SpriteDrawer):
 
     map = ('player', 'player_bullet', 'player_hb', 'enemy', 'item',
            'enemy_bullet', 'ui', 'ui_element')
-
-
-# TODO
-def kill_player():
-    l = locator.tm['lives']
-    if l.value > 0:
-        l.value -= 1
-    else:
-        locator.scene_stack.pop()

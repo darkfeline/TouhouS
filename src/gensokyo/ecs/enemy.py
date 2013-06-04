cd1e14326bb344e6d33555e2d43a13bb13d9c2b3
Allen Li <darkfeline@abagofapples.com>
2013-06-03 22:46:42 -0700

    Fix enemy

--- src/gensokyo/ecs/enemy.py
+++ src/gensokyo/ecs/enemy.py
@@ -8,7 +8,7 @@ from gensokyo.ecs import sprite
 from gensokyo.primitives import Vector
 from gensokyo.ecs.bullet import make_bullet, RoundBullet
 from gensokyo.ecs.rails import Rails
-from gensokyo.ecs.script import Script
+from gensokyo.ecs.script import Script, Scriptlet
 from gensokyo.ecs import pos
 from gensokyo import resources
 
@@ -23,7 +23,7 @@ GenericEnemy = partial(
     life=200)
 
 
-def make_enemy(world, drawer, enemy, x, y, *, rails, script):
+def make_enemy(world, drawer, enemy, x, y, *, rails, scriptlets):
 
     e = world.make_entity()
     add = partial(world.add_component, e)
@@ -38,8 +38,12 @@ def make_enemy(world, drawer, enemy, x, y, *, rails, script):
     add(l)
     r = Rails(rails, (x, y))
     add(r)
-    assert isinstance(script, Script)
-    add(script)
+
+    if scriptlets:
+        s = Script()
+        for x in scriptlets:
+            s.add(x())
+        add(s)
 
     return e
 
@@ -60,20 +64,20 @@ class GrimReaper(ecs.System):
                 self.world.remove_entity(e)
 
 
-class LoopFireAtPlayer(Script):
+class LoopFireAtPlayer(Scriptlet):
 
     def __init__(self, rate):
         """rate is seconds per fire.  Smaller rate == faster"""
         self.state = 0
         self.rate = rate
 
-    def run(self, entity, world, root, dt):
+    def run(self, entity, world, master, dt):
         self.state += dt
         if self.state >= self.rate:
             player = world.tm['player']
             pos_ = world.cm[pos.Position]
             p = pos_[entity].pos
             v = Vector(*pos_[player].pos) - Vector(*p)
-            b = make_bullet(RoundBullet, p[0], p[1], v)
+            b = make_bullet(world, master.drawer, RoundBullet, p[0], p[1], v)
             world.gm['enemy_bullet'].add(b)
             self.state -= self.rate

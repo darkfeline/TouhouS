b2ffb6686d0efee22f8a5af34857d2e7394d3e99
Allen Li <darkfeline@abagofapples.com>
2013-06-04 20:01:12 -0700

    Fix data.stage

--- src/gensokyo/data/enemies/scripts.py
+++ src/gensokyo/data/enemies/scripts.py
@@ -1,7 +1,11 @@
+import logging
+
 from gensokyo.ecs.script import Scriptlet
 from gensokyo.primitives import Vector
 from gensokyo.ecs import pos
 from gensokyo.ecs.bullet import make_bullet
+
+logger = logging.getLogger(__name__)
 
 
 class LoopFireAtPlayer(Scriptlet):
@@ -28,3 +32,16 @@ class LoopFireAtPlayer(Scriptlet):
             b = make_bullet(world, master.drawer, self.bullet, p[0], p[1], v)
             world.gm['enemy_bullet'].add(b)
             self.state -= self.rate
+
+
+class TimedSuicide(Scriptlet):
+
+    def __init__(self, time):
+        self.state = 0
+        self.limit = time
+
+    def run(self, entity, world, master, dt):
+        self.state += dt
+        if self.state > self.limit:
+            logger.debug('TimedSuicide killed %r', entity)
+            world.remove_entity(entity)

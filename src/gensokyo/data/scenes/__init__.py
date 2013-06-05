e414d1b3692fde76b92317dd2f21dab4ea30f3f6
Allen Li <darkfeline@abagofapples.com>
2013-06-04 20:55:05 -0700

    Fix bug in scenes.start

--- src/gensokyo/data/scenes/__init__.py
+++ src/gensokyo/data/scenes/__init__.py
@@ -1,6 +1,6 @@
 from gensokyo.data.scenes import main_menu
 from gensokyo.data.scenes import game
 
-start = main_menu
+start = main_menu.MenuScene
 main_menu.MenuScene.transitions = {'exit': None, 'game': game.GameScene}
 game.GameScene.transitions = {'quit': main_menu.MenuScene}

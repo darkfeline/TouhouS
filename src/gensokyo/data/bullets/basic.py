c7ad4c92b834e540133debeae359d283526befbd
Allen Li <darkfeline@abagofapples.com>
2013-06-04 17:43:33 -0700

    Rename bullet image resource file

--- src/gensokyo/data/bullets/basic.py
+++ src/gensokyo/data/bullets/basic.py
@@ -3,4 +3,4 @@ from gensokyo import resources
 from gensokyo.data.bullets import EnemyBullet
 
 BasicRedBullet = EnemyBullet(
-    img=resources.bullet['round'], hitbox=primitives.Circle(0, 0, 10))
+    img=resources.bullet['basic_red'], hitbox=primitives.Circle(0, 0, 10))

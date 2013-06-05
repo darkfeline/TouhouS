f747112fb7fc884a7fe00499cec3e1f6284325e6
Allen Li <darkfeline@abagofapples.com>
2013-06-04 17:52:23 -0700

    Dynamically load resources

--- src/gensokyo/resources.py
+++ src/gensokyo/resources.py
@@ -18,6 +18,10 @@ def _center(image):
     image.anchor_x = image.width // 2
     image.anchor_y = image.height // 2
 
+__all__ = [
+    'ui_image', 'icon16', 'icon32', 'star',
+    'players', 'enemies', 'bullets'
+]
 logger = logging.getLogger(__name__)
 
 logger.info("Initializing resources...")
@@ -33,23 +37,23 @@ icon32 = resource.image('icon32.png')
 star = resource.image('star.png')
 
 # Players
-player = {}
-a = "players"
-# Reimu
-player['reimu'] = {}
-b = a + "/reimu"
-player['reimu']['player'] = centered_image(b + "/player.png")
-player['reimu']['shot'] = centered_image(b + '/shot.png')
-player['reimu']['hitbox'] = centered_image(b + '/hitbox.png')
+players = {}
+a = os.path.join(resource.path, 'players')
+for player in os.listdir(a):
+    current = {}
+    players[player] = current
+    b = os.path.join(a, player)
+    for x in os.listdir(b):
+        players[os.path.basename(x)] = centered_image(os.path.join(b, x))
 
 # Enemies
-enemy = {}
-a = "enemies"
-enemy['generic'] = centered_image(a + '/generic.png')
+enemies = {}
+for x in os.listdir(os.path.join(resource.path, 'enemies')):
+    enemies[os.path.basename(x)] = centered_image(os.path.join('enemies', x))
 
 # Bullets
-bullet = {}
-a = "bullets"
-bullet['round'] = centered_image(a + '/round.png')
+bullets = {}
+for x in os.listdir(os.path.join(resource.path, 'bullets')):
+    bullets[os.path.basename(x)] = centered_image(os.path.join('bullets', x))
 
 logger.info("Finished initializing resources.")

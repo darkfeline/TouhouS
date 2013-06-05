7523e49c8d87eb3568dff333b1c078613427493b
Allen Li <darkfeline@abagofapples.com>
2013-06-04 20:59:24 -0700

    Fix resource loading bugs

--- src/gensokyo/data/stages/stage_one.py
+++ src/gensokyo/data/stages/stage_one.py
@@ -3,7 +3,10 @@ import logging
 from gensokyo.ecs import enemy
 from gensokyo import stage
 from gensokyo.globals import GAME_AREA
-from gensokyo import data
+from gensokyo.data.enemies.basic import BasicFairy
+from gensokyo.data.enemies import scripts
+from gensokyo.data.bullets.basic import BasicRedBullet
+
 
 logger = logging.getLogger(__name__)
 
@@ -34,11 +37,11 @@ class LoopSpawnEnemy(stage.Script):
             self.state -= self.rate
             r = (('straight', (GAME_AREA.left - 30, 300), 5),)
             e = enemy.make_enemy(
-                stage.world, stage.master.drawer, data.enemies.basic.Fairy,
+                stage.world, stage.master.drawer, BasicFairy,
                 self.pos[0], self.pos[1], rails=r,
                 scriptlets=[
-                    data.enemies.script.TimedSuicide(6),
-                    data.enemies.script.LoopFireAtPlayer(0.3, 7)
+                    scripts.TimedSuicide(6),
+                    scripts.LoopFireAtPlayer(0.3, 7, BasicRedBullet)
                 ]
             )
             logger.debug('spawned %r', e)

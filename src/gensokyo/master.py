7064d8b7c9e83b8dd5507fdfb65e1a9d64bf4d51
Allen Li <darkfeline@abagofapples.com>
2013-06-12 22:26:32 -0400

    Fix master

--- src/gensokyo/master.py
+++ src/gensokyo/master.py
@@ -3,12 +3,15 @@ import abc
 __all__ = ['Master']
 
 
+def _make_getter(name):
+    def getter(self):
+        return getattr(self, name, None)
+    return getter
+
+
 class Master(metaclass=abc.ABCMeta):
     pass
 
 for x in ('rootenv', 'statem', 'drawer', 'clock'):
-    y = '_' + x
-    def getter(self):
-        return getattr(self, y, None)
+    getter = _make_getter('_' + x)
     setattr(Master, x, property(getter))
-del x, y, getter

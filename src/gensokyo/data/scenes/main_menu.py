e560fa1f8f6f33b0877d3f5ce57e7a019ee5c6f6
Allen Li <darkfeline@abagofapples.com>
2013-07-07 19:03:37 -0400

    Change main_menu.py

--- src/gensokyo/data/scenes/main_menu.py
+++ src/gensokyo/data/scenes/main_menu.py
@@ -1,54 +1,75 @@
 import logging
 
-from pyglet.window import key
-
 from gensokyo import state
 from gensokyo import sprite
 from gensokyo import gvars
-from gensokyo.sprite import SpriteDrawer
+from gensokyo import menu
+from gensokyo.data import stages
 
 logger = logging.getLogger(__name__)
 
 
-class MenuScene(state.Scene):
+class MenuScene(menu.MenuMaster, state.Scene):
 
     def __init__(self, master):
-
         logger.info("Initializing MenuScene...")
         super().__init__(master)
-        self._drawer = MenuDrawer()
-        self.input = MenuInput(master.statem)
-
         logger.debug("Making Label...")
         self.title = sprite.Label(
-            self.drawer, 'text', x=20, y=gvars.HEIGHT - 30,
+            self.drawer, 'menu', x=20, y=gvars.HEIGHT-30,
             text="Welcome to TouhouS", color=(255, 255, 255, 255))
+        self._statem = MainMenu(self, 50, gvars.HEIGHT-100)
+        self.statem.init(MainPane)
 
     def enter(self):
         logger.info("Entering MenuScene...")
-        self.rootenv.window.push_handlers(self.input)
         self.master.drawer.add(self.drawer)
 
     def exit(self):
         logger.info("Exiting MenuScene...")
-        self.rootenv.window.remove_handlers(self.input)
         self.master.drawer.remove(self.drawer)
 
+    def event_start(self, *args, **kwargs):
+        self.master.statem.event('game', *args, **kwargs)
 
-class MenuDrawer(SpriteDrawer):
+    def event_exit(self):
+        self.master.statem.event('exit')
 
-    layers = ('bg', 'text')
+
+class MainMenu(menu.Menu):
+
+    def hook_start(self, *args):
+        self.event('exit')
+        self.master.event_start(*args)
+
+    def hook_exit(self):
+        self.event('exit')
+        self.master.event_exit()
 
 
-class MenuInput:
+class MainPane(menu.MenuPane):
 
-    def __init__(self, statem):
-        self.statem = statem
+    keys = ('Start', 'Exit')
+    map = {
+        'Start': 'stage_select',
+        'Exit': 'hook_exit'
+    }
 
-    def on_key_press(self, symbol, modifiers):
-        if symbol == key.ESCAPE:
-            logger.info('Exiting menu')
-            self.statem.event('exit')
-        elif symbol == key.SPACE:
-            logger.info('Entering game')
-            self.statem.event('game')
+
+class StageSelectPane(menu.MenuPane):
+
+    keys = ('Stage One', 'Back')
+    map = {
+        'Stage One': ('hook_start', stages.stage_one.StageOne),
+        'Back': 'back'
+    }
+
+MainPane.transitions = {
+    'exit': None,
+    'stage_select': StageSelectPane
+}
+
+StageSelectPane.transitions = {
+    'back': MainPane,
+    'exit': None
+}
